fb2f73d8f600f337df116540e4f7694780bdc47a
Marcus  Roza <mavirolero@gmail.com>
2024-01-09 14:02:20 -0300

    Add createSupplieDTO interface

---
+++ src/infra/http/models/Supplie/create.ts
@@ -0,0 +1,9 @@
+export interface createSupplieDTO {
+  name: string;
+  logo: string;
+  state_abbreviation: string;
+  cost_per_kwh: string;
+  min_kwh_limit: string;
+  total_customers: string;
+  average_customer_rating: string;
+}
